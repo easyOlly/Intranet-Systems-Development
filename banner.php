a5d1efd3d9ce1c02d2a87941c11ffc17646a8bd5
easyolly <o.olagunju@rgu.ac.uk>
2023-03-24 10:56:10 +0000

    new

--- banner.php
+++
@@ -1,4 +0,0 @@
-<div class="login_sign_up">
-		<a rel="tooltip"  data-placement="left" title="Click Here to Login" id="login" href="login.php"  class="btn btn-info btn-large"><i class="icon-signin icon-large"></i>&nbsp;Login</a>
-  <p><a rel="tooltip"  data-placement="bottom" title="Click Here to Sign UP" id="signup" href="signup.php">Dont have an Account? Sign Up Now</a></p>
-</div>
