07f866e31b00dec87696ae2d665c9d76579079f2
easyolly <o.olagunju@rgu.ac.uk>
2023-03-26 22:44:35 +0100

    new

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
 <head>
 <meta charset="utf-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">  
+<meta http-equiv="X-UA-Compatible" content="IE=edge">       
 <title>Login</title>
 <!-- Bootstrap stylesheet -->
 <link href="new/bootstrap/css/bootstrap.css" rel="stylesheet">
