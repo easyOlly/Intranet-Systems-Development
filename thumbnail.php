8339180f538b838840939f01d0d1caea5d2b4875
easyolly <o.olagunju@rgu.ac.uk>
2023-03-28 09:35:35 +0100

    new

--- thumbnail.php
+++ thumbnail.php
@@ -9,7 +9,7 @@
                                 
                                 <a  target="_blank" href="https://facebook.com/houseontherock" style="color:#FFF;"> <img src="img/32x32/facebook.png"></div><div class="fb">Like us On facebook</div>			</a> </div>
                                 
-     <div class="social_index">
+     <div class="social_index">   
 <div class="mm">
                                 
                                <a target="_blank" href="https://twitter.com/houseontherockc" style="color:#fff;"> <img src="img/32x32/twitter.png"></div> <div class="twitter" style="width:115px;">Follow us on twitter</div>			</a> </div>                           
