3c266a1b50e47c50a43baae0a70e118b9d55e1be
easyolly <o.olagunju@rgu.ac.uk>
2023-04-04 12:46:35 +0100

    new

--- header.php
+++ header.php
@@ -15,11 +15,11 @@
             
                         
 	<!-- js -->			
-    <script src="js/jquery-1.7.2.min.js"></script>
+    <script src="js/jquery-1.7.2.min.js"></script>    
     <script src="js/bootstrap.js"></script>
     <script src="js/jquery.hoverdir.js"></script>
     
-    <script type="text/javascript" src="js/cufon-yui.js"></script>
+    <script type="text/javascript" src="js/cufon-yui.js"></script>   
 <script type="text/javascript" src="js/cufon-titillium-900.js"></script>
 <script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
 <script type="text/javascript" src="js/script.js"></script>
